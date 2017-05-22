8254ccc178d7015520f8e54baca6f6c986bbda33
Joshua Fairchild and Marilyn Carlin <joshua+marilyn@>
2017-05-22 10:31:25 -0700

    add Parcel class with properties for length,width,height,weight and constructor fn

---
+++ Objects/Parcel.cs
@@ -0,0 +1,18 @@
+namespace Parcel.Objects
+{
+  public class Parcel
+  {
+    public int Length {get; set;}
+    public int Width {get; set;}
+    public int Height {get; set;}
+    public int Weight {get; set;}
+
+    public Parcel(int length, int width, int height, int weight)
+    {
+      Length = length;
+      Width = width;
+      Height = height;
+      Weight = weight;
+    }
+  }
+}
